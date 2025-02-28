12c4497a98dc0eadd4b6997cde4ab6bfdd2f2999
m3schl8 <bandar984@gmail.com>
2025-03-01 01:46:47 +0300

    تم تشغيله
    تم تشغيله 100٪

---
+++ Blue secren.py
@@ -0,0 +1,25 @@
+# Define window size
+window_width = 1920
+window_height = 1080
+# Function to print text in blue
+#func 
+#from colorama import colored
+def print_blue (text) :
+#del getattr_Library sum/00 
+ print("Library/numby")
+# Example usage of the function
+
+ #int foot[]if text and"System/Library/Accounts/ 
+  #print([foot,text,getattr():
+# Define a function or class (assuming
+
+
+#in gool 
+set   is any and sum
+
+
+
+
+
+# Example usage of the class
+
